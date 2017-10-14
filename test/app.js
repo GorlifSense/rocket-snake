76abef5854aed8e0abdd9114286dbd9ce4c53a18
Fleischers <Fleischer458@gmail.com>
2017-10-14 20:05:35 +0300

    Fixed launched server

--- test/app.js
+++ test/app.js
@@ -4,9 +4,13 @@ const request = require('supertest');
 const app = require('../app.js');
 
 describe('GET /', () => {
-  it('should return 200 OK', (done) => {
+  it('should return 200 OK', (done)=> {
     request(app)
       .get('/')
       .expect(200, done);
+  });
+
+  after(() => {
+    app.close();
   });
 });
