1a6539f60cb24682c7cfaccb442bde705fcb3631
ArtemDerkach <artem.derkach.gs@gmail.com>
2017-10-28 21:09:06 +0300

    add game sockets. commented socket tests

--- app/utils/logger.js
+++ app/utils/logger.js
@@ -1,32 +1,32 @@
-'use strict';
-const winston = require('winston');
+// 'use strict';
+// const winston = require('winston');
 
-const logger = winston.createLogger({
-  level: process.env.LOG_LEVEL || 'verbose',
-  format: winston.format.json(),
-  transports: [
-    //
-    // - Write to all logs with level `info` and below to `combined.log`
-    // - Write all logs error (and below) to `error.log`.
-    //
-    new winston.transports.File({filename: 'error.log', level: 'error'}),
-    new winston.transports.File({filename: 'combined.log'})
-  ]
-});
+// const logger = winston.createLogger({
+//   level: process.env.LOG_LEVEL || 'verbose',
+//   format: winston.format.json(),
+//   transports: [
+//     //
+//     // - Write to all logs with level `info` and below to `combined.log`
+//     // - Write all logs error (and below) to `error.log`.
+//     //
+//     new winston.transports.File({filename: 'error.log', level: 'error'}),
+//     new winston.transports.File({filename: 'combined.log'})
+//   ]
+// });
 
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    level: 'silly',
-    colorize: true,
-    format: winston.format.combine(
-      winston.format.splat(),
-      winston.format.simple()
-    )
-  }));
-}
+// //
+// // If we're not in production then log to the `console` with the format:
+// // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+// //
+// if (process.env.NODE_ENV !== 'production') {
+//   logger.add(new winston.transports.Console({
+//     level: 'silly',
+//     colorize: true,
+//     format: winston.format.combine(
+//       winston.format.splat(),
+//       winston.format.simple()
+//     )
+//   }));
+// }
 
-module.exports = logger;
+// module.exports = logger;
